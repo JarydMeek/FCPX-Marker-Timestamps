43005919d4a19e312a76de6f2372b1db3a9ff04e
JarydMeek <jarydmeek@gmail.com>
2021-06-23 18:48:36 -0600

    After many hours, I've completed the drop frame/non drop frame to timecode converter. That should be the last major functionality piece. lots of styling and option work now :)

--- FCPX-Marker-Timestamps/ContentView.swift
+++ FCPX-Marker-Timestamps/ContentView.swift
@@ -10,21 +10,57 @@ import SwiftUI
 //Handler to actually process the FCPX XML File
 //I could treat this as an actual xml file, but in this case since the xml is so basic, it will be faster to just process it as a string.
 class dataHandler {
+    //Storage for markers
     var markers:[marker] = []
-    var start:Int = -1
-    var frameRate:Double = 0
+    
+    //Needed Project File Information
+    var projectName:String = "No Project Selected"
+    var frameRate: Double = 0
     
     struct marker {
+        var hours:Int = 0
+        var minutes:Int = 0
         var seconds:Int = 0
         var frames:Int = 0
         var name:String = ""
         let id = UUID()
     }
     
+    //A lot of the logic for this function came from here
+    //http://www.andrewduncan.net/timecodes/
+    
+    func dropFrameMath(inFrames: Int, inRate: Double) -> (Int, Int, Int, Int){
+        let rawFrames = inFrames
+        let roundedFrameRate = Int(inRate.rounded())
+        var finalFrames: Double
+        if inRate != inRate.rounded() {
+            
+            let tenMins = Int(rawFrames / (600 * roundedFrameRate))
+            let mins = Int(rawFrames / (60 * roundedFrameRate))
+            
+            let addedFrames = (2 * mins) + (-2 * tenMins)
+            
+             finalFrames = Double(rawFrames + addedFrames)
+        } else {
+             finalFrames = Double(rawFrames)
+        }
+        
+        let hours = Int((((finalFrames / Double(roundedFrameRate)) / 60) / 60).truncatingRemainder(dividingBy: 60))
+        let minutes = Int(( (finalFrames / Double(roundedFrameRate)) / 60).truncatingRemainder(dividingBy: 60))
+        let seconds = Int((finalFrames / Double(roundedFrameRate)).truncatingRemainder(dividingBy: 60))
+        let frames = Int(finalFrames.truncatingRemainder(dividingBy: Double(roundedFrameRate)))
+        
+        return (hours,minutes,seconds,frames)
+        
+    }
+
+    
     func divideNums(input: String) -> Double {
-        let value = input.dropLast()
-        var firstNumStr = value[...value.firstIndex(of: "/")!]
-        var secondNumStr = value[value.firstIndex(of: "/")!...]
+        if input == "0" {
+            return 0
+        }
+        var firstNumStr = input[...input.firstIndex(of: "/")!]
+        var secondNumStr = input[input.firstIndex(of: "/")!...]
         firstNumStr.removeLast()
         secondNumStr.removeFirst()
         if let firstNum = Double(firstNumStr), let secondNum = Double(secondNumStr) {
@@ -33,16 +69,27 @@ class dataHandler {
             return -1
         }
     }
+
+    //Here's What I need
+    //Overall Framerate
+    //Project Name
+    //
     
     func openFile(URL: String) -> Bool {
+        //Empty Marker Array so that we start fresh
         markers = []
+        //Try to open the file
         if let contents = try? String(contentsOfFile: String(URL.dropFirst(7))) {
+            //Load Project Name
+            if let range = contents.range(of: "project name=\"") {
+                projectName = String(contents[range.upperBound...contents.index(contents[contents.index(range.upperBound, offsetBy: 1)...].range(of: "\" uid=\"")!.lowerBound, offsetBy: -1)])
+            }
             //FRAME RATE
             if let range = contents.range(of: "frameDuration") {
                 let index = contents.index(range.upperBound, offsetBy: 2)
                 var temp = contents[index...contents[index...].firstIndex(of: "\"")!]
-                temp.removeLast(1)
-                frameRate = (Double(round(1000*(1/(divideNums(input: String(temp)))))/1000))
+                temp.removeLast(2)
+                frameRate = (Double(round(1000*(1/(divideNums(input: (String(temp))))))/1000))
                 var current = contents[index...]
                 //START VALUE
                 if let range2 = contents.range(of: "<spine>") {
@@ -51,31 +98,52 @@ class dataHandler {
                 if let range3 = current.range(of: " start=\"") {
                     var temp = contents[range3.upperBound...contents[range3.upperBound...].firstIndex(of: "\"")!]
                     temp.removeLast(2)
-                    start = Int(temp) ?? -1
                     current = current[range3.upperBound...]
                 }
-                //BEGIN MARKERS
+                //At This Point, We have the FPS and Project Name. Awesome.
                 
+                //BEGIN MARKERS
                 while current.contains("<marker start=\""){
-                    let currIndex = current.range(of: "<marker start=\"")!.upperBound
-                    var temp = current[currIndex...]
-                    //print(temp)
-                    temp = temp[...temp.firstIndex(of: "s")!]
-                    let dividedNums = (divideNums(input: String(temp))-Double(start))
-                    let decimal = dividedNums.truncatingRemainder(dividingBy: 1)
-                    let seconds = Int(dividedNums)
-                    let frames = Int(round(decimal*frameRate))
-                    
-                    let valIndex = current.range(of: " value=\"")!.upperBound
-                    let endRange = current.range(of: "\"/>")!
-                    var value = current[valIndex...endRange.lowerBound]
-                    value.removeLast()
-                    
-                    let tempMarker = marker(seconds: seconds, frames: frames, name: String(value))
-                    markers.append(tempMarker)
-                    current = current[endRange.upperBound...]
+                    if let markerRange = current.range(of: "<marker start=\"") {
+                        
+                        //Get the Reference Clip information needed for the calculation
+                        let refClipRange = current[...markerRange.lowerBound].range(of:"<ref-clip name=\"",  options: String.CompareOptions.backwards)
+                        let offsetRange = current[refClipRange!.upperBound...markerRange.upperBound].range(of:"offset=\"")
+                        let startRange = current[refClipRange!.upperBound...markerRange.upperBound].range(of:"start=\"")
+                        let offset = current[offsetRange!.upperBound...current.index(current[offsetRange!.upperBound...].firstIndex(of: "\"")!, offsetBy: -2)]
+                        let start = current[startRange!.upperBound...current.index(current[startRange!.upperBound...].firstIndex(of: "\"")!, offsetBy: -2)]
+                        
+                        
+                        //Get the information needed for the marker point calculation.
+                        let endIndex = current.index(current[markerRange.upperBound...].firstIndex(of: "\"")!, offsetBy: -2)
+                        let markerPoint = current[markerRange.upperBound...endIndex]
+                        
+                        //Get the marker name
+                        let valueRange = current[markerRange.upperBound...].range(of:"value=\"")
+                        let endValue = current.index(current[valueRange!.upperBound...].range(of: "\"/>")!.lowerBound, offsetBy: -1)
+                        let value = current[valueRange!.upperBound...endValue]
+                        
+                        //We have all the information we need. Lets convert these to timecodes.
+                        
+                        //divide the numbers and do the drop frame math
+                        //Calculate raw number
+                        let rawTimeCode = (divideNums(input: String(markerPoint)) - divideNums(input: String(start))) + divideNums(input: String(offset))
+                        var rawFrames = (rawTimeCode * frameRate)
+                        rawFrames.round()
+                        let (hours, mins, seconds, frames) = dropFrameMath(inFrames:Int(rawFrames), inRate: frameRate)
+                        
+                        var tempMarker = marker()
+                        tempMarker.hours = hours
+                        tempMarker.minutes = mins
+                        tempMarker.seconds = seconds
+                        tempMarker.frames = frames
+                        tempMarker.name = String(value)
+                        
+                        markers.append(tempMarker)
+                        //reset string for next iteration
+                        current = current[endValue...]
+                    }
                 }
-                print(markers)
             } else {
                 return false
             }
@@ -85,11 +153,11 @@ class dataHandler {
         }
     }
     func getDataString(URL: String) -> String {
-        print("yo")
+
         var storage = ""
         if data.openFile(URL: URL) {
             for marker in markers {
-                storage += "\(marker.seconds):\(marker.frames) - \(marker.name)\n"
+                storage += "\(marker.minutes):\(marker.seconds):\(marker.frames) - \(marker.name)\n"
             }
             return storage
         } else {
@@ -130,7 +198,7 @@ struct ContentView: View {
         open.allowedFileTypes = ["fcpxml"]
         open.begin { response in
             if response == NSApplication.ModalResponse.OK, let fileUrl = open.url {
-                openURL = fileUrl.absoluteString
+                openURL = fileUrl.absoluteString.removingPercentEncoding!
             }
         }
     }
@@ -142,7 +210,7 @@ struct ContentView: View {
                     .padding()
                     .multilineTextAlignment(.center)
                     .font(.largeTitle)
-                Text(openURL[openURL.index(openURL.lastIndex(of: "/")!,offsetBy: 1)...])
+                Text("No File Selected")
                 Button(action: {
                     openFile()
                 }, label: {
@@ -153,15 +221,22 @@ struct ContentView: View {
                         .padding(.trailing, 20)
                 }).buttonStyle(MainButton())
                 
-                if openURL != "/No File Selected" {//If file is selected, show extra options
-                    HStack{
-                        Spacer()
-                        ScrollView{
-                            VStack{
-                                Text(data.getDataString(URL: openURL))
-                            }.frame(minWidth: metrics.size.width*0.8)
-                        }.background(Color.white)
-                        Spacer()
+                if data.openFile(URL: openURL) {//If file is selected, show extra options
+                    VStack{
+                        VStack{
+                            Text("Project Name:")
+                            Text(data.projectName)
+                        }
+                        HStack{
+                            Spacer()
+                            ScrollView{
+                                VStack{
+                                    Text(data.getDataString(URL: openURL))
+                                }.frame(minWidth: metrics.size.width*0.8)
+                            }.background(Color.white)
+                            Spacer()
+                        }
+                            
                     }
                 }
             }.padding(20)
